a50c17ef17141e6be043e6f4fb7083dcb2b055f5
Admin <Tgafvert@gmail.com>
2017-07-10 09:51:56 -0400

    did the thangzz

--- fundamentals.js
+++ fundamentals.js
@@ -2,24 +2,31 @@
 
 // Create an array of strings called `colors` that contains three colors.
 
-
+var colors = ["red", "blue", "orange"]
 
 // Access the last item in the array and assign to a variable called `last`.
 
-
+var last = colors[colors.length-1]
 
 // Create an empty array called `favoriteColors`.
 
+favoriteColors = []
 
 
 // Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 
-
+for (i = 0; i > colors.length; i++) {
+  favoriteColors.push(colors[i])
+}
 
 // Create an object literal called `instructor` that contains three key-value pairs.
 
-
-
+var instructor = {
+  name: "Andy",
+  age: "21",
+  gender: "male"
+}
 // Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
+instructor['facial-hair'] = true
--- hof.js
+++ hof.js
@@ -10,9 +10,38 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peoplesAges`.
 
+var peoplesAges = people.map(function(ages) {
+  return ages.age
+})
 
 
 
 // Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are older than 35. Assign the returned array
 // to a variable called `peopleOlderThan35`.
+peopleOlderThan35 = people.filter(function (oldAge) {
+  var age35 = 35
+  return parseInt(oldAge.age) > age35
+})
+
+//
+// var names = states.map(function(state) {
+//   return state.capital + ', ' + state.name
+// })
+//
+// let consonantCapitals = state.filter(function (state) {
+//   const firstLetter = state.capital.charAt(0)
+//   return !['A','E','I','O','U','Y'].includes(firstLetter)
+//
+// })
+//
+// let twoWordStates = states.filter(function (state) {
+//   return state.name.includes(' ')
+// }).map(function (state) {
+//   return state.name
+// })
+//
+// let northOfAnnapolis = states.filter(function (state) {
+//   var latOfAnnapolis = 38.972945
+//   return parseFloat(state.lat) > latOfAnnapolis
+// })
